e98dc6fcc170392203dde59f3fbb0d2d4d5e2826
Chen Yu Pao <windperson@gmail.com>
2018-02-27 03:55:41 +0800

    Android example

---
+++ AndroidPrj/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using AndroidPrj.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace AndroidPrj
+{
+    [Activity(Label = "AndroidPrj", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AppCompatActivity
+    {
+        public static MainActivity Instance;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Forms.Init(this, savedInstanceState);
+            Instance = this;
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "Phoneword";
+
+            var phonewordPage = new PhonewordPage();
+            var mainPage = phonewordPage.CreateFragment(this);
+            FragmentManager
+                .BeginTransaction()
+                .Replace(Resource.Id.fragment_frame_layout, mainPage)
+                .Commit();
+
+            FragmentManager.BackStackChanged += (sender, e) =>
+            {
+                bool hasBack = FragmentManager.BackStackEntryCount > 0;
+                SupportActionBar.SetHomeButtonEnabled(hasBack);
+                SupportActionBar.SetDisplayHomeAsUpEnabled(hasBack);
+                SupportActionBar.Title = hasBack ? "Call History" : "Phoneword";
+            };
+        }
+
+        public override bool OnOptionsItemSelected(Android.Views.IMenuItem item)
+        {
+            if (item.ItemId == global::Android.Resource.Id.Home && FragmentManager.BackStackEntryCount > 0)
+            {
+                FragmentManager.PopBackStack();
+                return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public void NavigateToCallHistoryPage()
+        {
+            var callHistoryPage = new CallHistoryPage().CreateFragment(this);
+            FragmentManager
+                .BeginTransaction()
+                .AddToBackStack(null)
+                .Replace(Resource.Id.fragment_frame_layout, callHistoryPage)
+                .Commit();
+        }
+    }
+}
+
---
+++ AndroidPrj/Views/PhonewordPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AndroidPrj.Models;
+using AndroidPrj.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidPrj.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PhonewordPage : ContentPage
+	{
+	    string _translatedNumber;
+
+	    public PhonewordPage()
+	    {
+	        InitializeComponent();
+	    }
+
+	    void OnTranslate(object sender, EventArgs e)
+	    {
+	        _translatedNumber = PhonewordTranslatorService.ToNumber(phoneNumberText.Text);
+	        if (!string.IsNullOrWhiteSpace(_translatedNumber))
+	        {
+	            callButton.IsEnabled = true;
+	            callButton.Text = "Call " + _translatedNumber;
+	        }
+	        else
+	        {
+	            callButton.IsEnabled = false;
+	            callButton.Text = "Call";
+	        }
+	    }
+
+	    async void OnCall(object sender, EventArgs e)
+	    {
+	        if (await this.DisplayAlert(
+	            "Dial a Number",
+	            "Would you like to call " + _translatedNumber + "?",
+	            "Yes",
+	            "No"))
+	        {
+	            var dialer = new PhoneDialerService();
+	            if (dialer != null)
+	            {
+	                AppModel.PhoneNumbers.Add(_translatedNumber);
+	                callHistoryButton.IsEnabled = true;
+	                dialer.Dial(_translatedNumber);
+	            }
+	        }
+	    }
+
+	    void OnCallHistory(object sender, EventArgs e)
+	    {
+	        MainActivity.Instance.NavigateToCallHistoryPage();
+	    }
+    }
+}
