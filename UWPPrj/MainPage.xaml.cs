08c2b981741115823739cb4a69490e1c25aac363
Chen Yu Pao <windperson@gmail.com>
2018-02-28 11:35:38 +0800

    UWP example

---
+++ UWPPrj/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPPrj.Views;
+using Xamarin.Forms.Platform.UWP;
+using Page = Windows.UI.Xaml.Controls.Page;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPPrj
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage Instance;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Enabled;
+            
+            Instance = this;
+            var phonewordPage = new PhonewordPage();
+            FrameworkElement uwpFrameworkElement = phonewordPage.CreateFrameworkElement();
+            this.Content = uwpFrameworkElement;
+        }
+
+        public void NavigateToCallHistoryPage()
+        {
+            this.Frame.Navigate(new CallHistoryPage());
+        }
+    }
+}
---
+++ UWPPrj/Models/AppModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPPrj.Models
+{
+    class AppModel
+    {
+        static List<string> phoneNumbers;
+
+        public static List<string> PhoneNumbers
+        {
+            get
+            {
+                if (phoneNumbers == null)
+                {
+                    phoneNumbers = new List<string>();
+                }
+                return phoneNumbers;
+            }
+            set
+            {
+                phoneNumbers = value;
+            }
+        }
+    }
+}
